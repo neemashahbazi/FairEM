d2e3abe35effb5e6183b5580e2f7c71a5d6d9f7f
ndanevski1 <ndanevsk@u.rochester.edu>
2022-08-31 11:08:55 -0400

    Added combined graphs for unfairness

--- src/run.py
+++ src/run.py
@@ -1,11 +1,22 @@
 from preprocessing import run_deepmatcher, jsonl_to_predictions, deepmatcher_output_to_predictions
 from pprint import pprint
 import workloads as wl
+import matplotlib.lines as mlines
 import pandas as pd
 import FairEM as fem
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import matplotlib.patches as mpatches
 from measures import *
+
+
+def save_pandas_csv_if_not_exists(dataframe, outname, outdir):
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    fullname = os.path.join(outdir, outname)
+    dataframe.to_csv(fullname, index=False)
+
 
 def make_acronym(word, delim):
     res = ""
@@ -169,9 +180,102 @@ def create_distance_to_bin(distances, n = 4):
     
     return distance_to_bin
 
+def plot_one_dataset_all_measures(dataset, experiment):
+    models = ["deepmatcher", "ditto", "GNEM", "HierMatch", "MCAN", "SVM", \
+        "RuleBasedMatcher", "RandomForest", "NaiveBayes", "LogisticRegression", "LinearRegression", "Dedupe", "DecisionTree"]
+    non_neural = set(["SVM", "RuleBasedMatcher", "RandomForest", "NaiveBayes", "LogisticRegression", "LinearRegression", "Dedupe", "DecisionTree"])
+    neural = set(["deepmatcher", "ditto", "GNEM", "HierMatch", "MCAN"])
+    colors = ["red", "blue", "orange", "green", "purple", "black", "yellow", "pink", "gray", "cyan", "lightgreen", "brown", "midnightblue"]
+
+    dataframes = []
+    for model in models:
+        df = pd.read_csv("../experiments/" + dataset + "/results/" + model + "_results_experiment" + experiment + ".csv")
+        dataframes.append(df)
+
+    num_of_measures = 9 if experiment == "1" else 6
+
+    sens_attributes = dataframes[0]["sens_attr"].to_list()
+    x = sens_attributes[:int(len(sens_attributes)/num_of_measures)]
+    print("x = ", x)
+    measures = dataframes[0]["measure"].to_list()
+    y = measures[::int(len(measures) / num_of_measures)]
+    print("y = ", y)
+
+    fig, ax = plt.subplots()
+    fig.canvas.draw()
+    plt.rcParams["figure.figsize"] = [17, 17]
+    plt.rcParams["figure.autolayout"] = True
+    plt.xlim(0,len(x))
+    plt.ylim(0,len(y))
+    # plt.grid()
+    xticks = np.arange(0.5, len(x)+0.5, 1).tolist()
+    print("xticks = ", xticks)
+    ax.set_xticks(xticks)
+    yticks = np.arange(0.5, len(y)+0.5, 1).tolist()
+    print("yticks = ", yticks)
+    ax.set_yticks(yticks)
+    ax.set_xticklabels(x, fontsize=5, rotation=45)
+    ax.set_yticklabels([make_acronym(a, "_") for a in y], fontsize=5)
+
+    x_offset = [0.0, 0.0, 0.0, 0.2, 0.2, 0.2, -0.2, -0.2, -0.2, 0.3, 0.3, -0.3, -0.3]
+    y_offset = [0.0, 0.2, -0.2, 0.0, 0.2, -0.2, 0.0, 0.2, -0.2, 0.3, -0.3, 0.3, -0.3]
+
+    print("len(dataframes) = ", len(dataframes))
+    for i in range(len(dataframes)):
+        df = dataframes[i]
+        for index, row in df.iterrows():
+            # plotting only unfair
+            if(row["is_fair"] == True):
+                continue
+            x_index = x.index(row["sens_attr"])
+            y_index = y.index(row["measure"])
+            marker = "o" if models[i] in neural else "x"
+            plt.plot(x_index + 0.5 + x_offset[i], y_index + 0.5 + y_offset[i], marker=marker, markersize=5, markeredgecolor=colors[i], markerfacecolor=colors[i])
+
+    # customize legend
+    red_patch = mpatches.Patch(color='red', label="deepmatcher")
+    blue_patch = mpatches.Patch(color='blue', label="ditto")
+    orange_patch = mpatches.Patch(color='orange', label="GNEM")
+    green_patch = mpatches.Patch(color='green', label="HierMatch")
+    purple_patch = mpatches.Patch(color='purple', label="MCAN")
+    black_patch = mpatches.Patch(color='black', label="SVM")
+    yellow_patch = mpatches.Patch(color='yellow', label="RuleBasedMatcher")
+    pink_patch = mpatches.Patch(color='pink', label="RandomForest")
+    gray_patch = mpatches.Patch(color='gray', label="NaiveBayes")
+    cyan_patch = mpatches.Patch(color='cyan', label="LogisticRegression")
+    lightgreen_patch = mpatches.Patch(color='lightgreen', label="LinearRegression")
+    brown_patch = mpatches.Patch(color='brown', label="Dedupe")
+    midnightblue_patch = mpatches.Patch(color='midnightblue', label="DecisionTree")
+    cross_patch = mlines.Line2D([], [], color='black', marker="x", linestyle='None', markersize=5, label="Non-neural")
+    circle_patch = mlines.Line2D([], [], color='black', marker="o", linestyle='None', markersize=5, label="Neural")
+
+    plt.legend(handles=[red_patch, blue_patch, orange_patch, green_patch, purple_patch, black_patch, \
+        yellow_patch, pink_patch, gray_patch, cyan_patch, lightgreen_patch, brown_patch, \
+        midnightblue_patch, cross_patch, circle_patch], \
+        fontsize = "5", bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0)
+    plt.tight_layout()
+
+
+
+    plt.savefig("../experiments/" + dataset + "/results/Unfairness_experiment" + experiment + ".png", dpi=200)
+    plt.close()
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+
 def experiment_one(model, dataset, left_sens_attribute, right_sens_attribute, 
                     epochs=10, single_fairness=True, 
-                    threshold=0.2, test_file = "test.csv"):
+                    threshold=0.2, test_file = "test.csv", plot_all_separately = False):
     if dataset == "itunes-amazon" or dataset == "shoes":
         test_file = "test_others.csv"
     workloads = run_one_workload(model, dataset, left_sens_attribute, right_sens_attribute, 
@@ -190,52 +294,53 @@ def experiment_one(model, dataset, left_sens_attribute, right_sens_attribute,
                 "false_omission_rate_parity"]
     measures_acronymes = [make_acronym(x, "_") for x in measures]
     
-    aggregate = "distribution"
-    for measure in measures:
-        is_fair = fairEM.is_fair(measure, aggregate)
-        is_fair_real = fairEM.is_fair(measure, aggregate, real_distr = True)
-        binary_fairness.append(is_fair)
-        actual_fairness.append(is_fair_real)
-
-    
-
     attribute_names = []
     for k_comb in workloads[0].k_combs_to_attr_names:
         attribute_names.append(workloads[0].k_combs_to_attr_names[k_comb])
-    
 
-    # if dataset == "itunes-amazon" and model == "deepmatcher":
-        # print("attributes = ", attribute_names)
-        # print("measures = ", measures)
-        # print("actual_fairness = ", actual_fairness)    
-        # print("itunes-amazon output on ditto")
-        # for i in range(len(measures)):
-            # print("measure = ", measures[i])
-            # for j in range(len(attribute_names)):
-                # print("attribute = ", attribute_names[j], "fairness = ", actual_fairness[i][j])
+    df = pd.DataFrame(columns=["measure","sens_attr","is_fair"])
 
-    figsize = (8,6)
-    x_font = 12
-    y_font = 12
-    
-    if dataset == "dblp-acm":
-        attribute_names = [make_acronym(x, " ") for x in attribute_names]
-        figsize = (8,8)
+    aggregate = "distribution"
+    for measure in measures:
+        temp_df = pd.DataFrame(columns=["measure","sens_attr","is_fair"])
+        is_fair = fairEM.is_fair(measure, aggregate)
+        is_fair_real = fairEM.is_fair(measure, aggregate, real_distr = True)
+        binary_fairness.append(is_fair)
+
+        temp_df["measure"] = [measure] * len(is_fair)
+        temp_df["sens_attr"] = attribute_names
+        temp_df["is_fair"] = is_fair
+
+        # save_pandas_csv_if_not_exists(dataframe, outname, outdir)
+        df = df.append(temp_df, ignore_index=True)
+        actual_fairness.append(is_fair_real)
+
+    save_pandas_csv_if_not_exists(dataframe = df, outname = model + "_results_experiment1.csv", outdir = "../experiments/" + dataset + "/results/")
+
+
+    if plot_all_separately:
+        figsize = (8,6)
         x_font = 12
         y_font = 12
-    elif dataset == "itunes-amazon":
-        figsize = (10, 7)
-        x_font = 9
-        y_font = 12        
-    
-    title = "Exp1: " + dataset + " " + model + " with " + str(threshold) + " threshold \nBinary Fairness Values For 1-subgroups and Single Fairness and 1 workload"
-    if dataset == "itunes-amazon" or dataset == "shoes":
-        title += " with others column"
-    plot_results_in_2d_heatmap(dataset, binary_fairness, attribute_names, 
-                                measures_acronymes, title, 
-                                figsize, x_font, y_font)
+        
+        if dataset == "dblp-acm":
+            attribute_names = [make_acronym(x, " ") for x in attribute_names]
+            figsize = (8,8)
+            x_font = 12
+            y_font = 12
+        elif dataset == "itunes-amazon":
+            figsize = (10, 7)
+            x_font = 9
+            y_font = 12        
+        
+        title = "Exp1: " + dataset + " " + model + " with " + str(threshold) + " threshold \nBinary Fairness Values For 1-subgroups and Single Fairness and 1 workload"
+        if dataset == "itunes-amazon" or dataset == "shoes":
+            title += " with others column"
+        plot_results_in_2d_heatmap(dataset, binary_fairness, attribute_names, 
+                                    measures_acronymes, title, 
+                                    figsize, x_font, y_font)
 
-def experiment_two(model, dataset, left_sens_attribute, right_sens_attribute, epochs=10, single_fairness=False, threshold=0.2):
+def experiment_two(model, dataset, left_sens_attribute, right_sens_attribute, epochs=10, single_fairness=False, threshold=0.2, plot_all_separately = False):
     test_file = "test.csv" if dataset == "dblp-acm" or dataset == "citation" else "test_others.csv"
     workloads = run_one_workload(model, dataset, left_sens_attribute, right_sens_attribute, 
                                 epochs=epochs, single_fairness=single_fairness, 
@@ -250,14 +355,15 @@ def experiment_two(model, dataset, left_sens_attribute, right_sens_attribute, ep
     measures_acronymes = [make_acronym(x, "_") for x in measures]
     
     aggregate = "distribution"
-    for i in range(len(measures)):
-        measure = measures[i]
-        is_fair = fairEM.is_fair(measure, aggregate)
-        binary_fairness.append(is_fair)
+    # for i in range(len(measures)):
+    #     measure = measures[i]
+    #     is_fair = fairEM.is_fair(measure, aggregate)
+    #     binary_fairness.append(is_fair)
         
-    # take only first 10 for example:
-    for i in range(len(binary_fairness)):
-        binary_fairness[i] = binary_fairness[i][:10]
+    # # take only first 10 for example:
+    # for i in range(len(binary_fairness)):
+    #     binary_fairness[i] = binary_fairness[i][:10]
+    actual_fairness = []
     attribute_names = []
     for k_comb in workloads[0].k_combs_to_attr_names:
         curr_attr_name = workloads[0].k_combs_to_attr_names[k_comb]
@@ -266,26 +372,47 @@ def experiment_two(model, dataset, left_sens_attribute, right_sens_attribute, ep
 
         attribute_names.append(curr_attr_name)
 
-    figsize = (8,6)
-    x_font = 12
-    y_font = 12
+        df = pd.DataFrame(columns=["measure","sens_attr","is_fair"])
 
-    if dataset == "dblp-acm":
-        attribute_names = [make_acronym(x, " ") for x in attribute_names]
-        figsize = (8,8)
+    aggregate = "distribution"
+    for measure in measures:
+        temp_df = pd.DataFrame(columns=["measure","sens_attr","is_fair"])
+        is_fair = fairEM.is_fair(measure, aggregate)
+        is_fair_real = fairEM.is_fair(measure, aggregate, real_distr = True)
+        binary_fairness.append(is_fair)
+
+        temp_df["measure"] = [measure] * len(is_fair)
+        temp_df["sens_attr"] = attribute_names
+        temp_df["is_fair"] = is_fair
+
+        # save_pandas_csv_if_not_exists(dataframe, outname, outdir)
+        df = df.append(temp_df, ignore_index=True)
+        actual_fairness.append(is_fair_real)
+
+    save_pandas_csv_if_not_exists(dataframe = df, outname = model + "_results_experiment2.csv", outdir = "../experiments/" + dataset + "/results/")
+
+
+    if plot_all_separately:
+        figsize = (8,6)
         x_font = 12
         y_font = 12
-    elif dataset == "itunes-amazon":
-        figsize = (5, 5)
-        x_font = 9
-        y_font = 12      
 
-    title = "Exp2: " + dataset + " " + model + " with " + str(threshold) + " threshold\nBinary Fairness Values For 1-subgroups and Pairwise Fairness and 1 workload - first 10"
-    if dataset == "itunes-amazon" or dataset == "shoes":
-        title += " with others column"
-    plot_results_in_2d_heatmap(dataset, binary_fairness, attribute_names[:10], 
-                                measures_acronymes, title, 
-                                figsize, x_font, y_font)
+        if dataset == "dblp-acm":
+            attribute_names = [make_acronym(x, " ") for x in attribute_names]
+            figsize = (8,8)
+            x_font = 12
+            y_font = 12
+        elif dataset == "itunes-amazon":
+            figsize = (5, 5)
+            x_font = 9
+            y_font = 12      
+
+        title = "Exp2: " + dataset + " " + model + " with " + str(threshold) + " threshold\nBinary Fairness Values For 1-subgroups and Pairwise Fairness and 1 workload - first 10"
+        if dataset == "itunes-amazon" or dataset == "shoes":
+            title += " with others column"
+        plot_results_in_2d_heatmap(dataset, binary_fairness, attribute_names[:10], 
+                                    measures_acronymes, title, 
+                                    figsize, x_font, y_font)
 
 def experiment_three(dataset, model, single_fairness=True, epochs=15, others=True):
     workloads = run_multiple_workloads(dataset, model, epochs=epochs, single_fairness=single_fairness, others=others)
@@ -629,18 +756,23 @@ def dataset_experiments(dataset, sens_att, epochs=10, single_fairness=True,
                     right_sens_attribute="right_" + sens_att, 
                     epochs=2, single_fairness=True,
                     threshold=threshold)
+    plot_one_dataset_all_measures(dataset, "1")
+    for mod in models:
         experiment_two(model=mod, dataset=dataset, 
                     left_sens_attribute="left_" + sens_att, 
                     right_sens_attribute="right_" + sens_att, 
                     epochs=2, single_fairness=False,
                     threshold=threshold)
+
+    plot_one_dataset_all_measures(dataset, "2")
+    
     
     
     
 def main():
     # citation_experiments()
-    # dataset_experiments("dblp-acm", "venue")
-    dataset_experiments("itunes-amazon", "Genre")
+    dataset_experiments("dblp-acm", "venue")
+    # dataset_experiments("itunes-amazon", "Genre")
     
     # full_experiment_one()
     # full_experiment_two()
